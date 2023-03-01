7ad3696ff4d0eb38c4713a2ee6390bbac19b8af1
mygiano <84497159+mygiano@users.noreply.github.com>
2023-03-02 03:50:48 +0800

    perdana

---
+++ data/Data_Bumil_8.js
@@ -0,0 +1,10 @@
+var json_Data_Bumil_8 = {
+"type": "FeatureCollection",
+"name": "Data_Bumil_8",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "fid": "2", "nama_bumil": "Anggelia Larenggam", "nik_bumil": "7109024408990002", "tmpt_tgl_bumil": "Paniki", "hamil_ke": 2.0, "hamil_anak_ke": 2.0, "riwayat_kehamilan": "Kehamilan 1 prematur", "tgl_lahir_bumil": "1999-08-04", "tgl_perk_lahir": "2023-04-09", "umur_bumil": 23.0, "usia_kehamilan": 22.0, "HPHT": "2022-07-03" }, "geometry": { "type": "Point", "coordinates": [ 125.361532723372349, 2.738217408409704 ] } },
+{ "type": "Feature", "properties": { "fid": "3", "nama_bumil": "Sindy Tumandihe", "nik_bumil": "7109036707970001", "tmpt_tgl_bumil": "Kel.Balehumara", "hamil_ke": 1.0, "hamil_anak_ke": 1.0, "riwayat_kehamilan": "-", "tgl_lahir_bumil": "1997-06-27", "tgl_perk_lahir": "2023-05-27", "umur_bumil": 25.0, "usia_kehamilan": 16.0, "HPHT": "2022-08-20" }, "geometry": { "type": "Point", "coordinates": [ 125.374713994331842, 2.350960353664928 ] } },
+{ "type": "Feature", "properties": { "fid": "4", "nama_bumil": "Astrid Tuwongadil", "nik_bumil": "7109036011030003", "tmpt_tgl_bumil": "Mahangiang", "hamil_ke": 1.0, "hamil_anak_ke": 1.0, "riwayat_kehamilan": "-", "tgl_lahir_bumil": "2003-11-20", "tgl_perk_lahir": "2023-01-08", "umur_bumil": 19.0, "usia_kehamilan": 36.0, "HPHT": "2022-04-03" }, "geometry": { "type": "Point", "coordinates": [ 125.390895344649593, 2.330397122811247 ] } }
+]
+}
